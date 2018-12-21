a28a728e521587480a850d6a77019a36a30498bf
zhengxiaosun <3166653897@qq.com>
2018-12-21 12:01:13 +0800

    hasRemaining() 方法判断原理（position < limit）

--- src/main/java/com/chapter3/nio/BufferBasicPrinciples.java
+++ src/main/java/com/chapter3/nio/BufferBasicPrinciples.java
@@ -11,20 +11,23 @@ public class BufferBasicPrinciples {
 
 	public static void main(String[] args) {
 		ByteBuffer byteBuffer = ByteBuffer.allocate(15);
+		System.err.println(byteBuffer.hasRemaining()); // true (return position < limit)
 		System.out.println("第一次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、0、15
 		for (int i = 0; i < 10; i++) {
 			byteBuffer.put((byte)i);
 		}
 		System.out.println("第二次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、10、15
 		byteBuffer.flip();
+		System.err.println(byteBuffer.hasRemaining()); // true
 		System.out.println("第三次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、0、10
-		for (int i = 0; i < 5; i++) {
+		while (byteBuffer.hasRemaining()) {
 			byteBuffer.get();
 		}
-		System.out.println("第四次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、5、10
+		System.out.println("第四次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、10、10
 		byteBuffer.flip();
-		System.out.println("第五次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、0、5
+		System.out.println("第五次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、0、10
 		byteBuffer.clear();
+		System.err.println(byteBuffer.hasRemaining()); // true
 		System.out.println("第六次：capacity="+byteBuffer.capacity()+", position="+byteBuffer.position()+", limit="+byteBuffer.limit()); // 15、0、15
 		byte[] originByte = new byte[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'};
 		byteBuffer.put(originByte); // java.nio.BufferOverflowException
